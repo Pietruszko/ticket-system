fa73c57c8a396cf58dbfaa84eeb36ca85d17fadb
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-04 21:34:10 +0200

    feat: add validation for status to ticket serializer, to prevent incorrect behaviour in backend although in frontend there will be no such options

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -6,5 +6,29 @@ class TicketSerializer(serializers.ModelSerializer):
         model = Ticket
         fields = ['id', 'title', 'body', 'created_at', 'updated_at', 'status']
         extra_kwargs = {
-            'created_at': {'read_only': True}
+            'created_at': {'read_only': True},
+            'updated_at': {'read_only': True}
         }
+
+    def validate_status(self, value):
+        # Prevent invalid transitions (draft to closed or in progress)
+        if self.instance and self.instance.status == 'D':
+            if value in ['I', 'C']:
+                raise serializers.ValidationError('Cannot close ticket that is in draft.')
+            
+        return value
+
+    def validate(self, data):
+        # Prevent changing status for closed tickets
+        if self.instance and self.instance.status == 'C':
+            raise serializers.ValidationError('Cannot change status of a closed ticket.')
+        
+        # Prevent creating tickets as closed or in progress
+        if not self.instance:
+            if data.get('status') in ['I', 'C']:
+                raise serializers.ValidationError('Cannot create tickets as closed or in progress')
+            
+        return data
+    
+    # def create(self, validated_data):
+        # Assign user on creating ticket
