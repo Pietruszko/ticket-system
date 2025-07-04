11bad7e438eefb9017c1a64ab924f9e2e5b1580c
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-04 16:22:17 +0200

    feat: create ticket viewset

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Ticket
+from .serializers import TicketSerializer
 
-# Create your views here.
+class TicketViewSet(ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
