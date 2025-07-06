48cab731e9afdcec2238f2b651e19c8f04647f4a
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 11:16:46 +0200

    feat: register ticket model into admin panel with custom ticket detail link method

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from .models import Ticket
 
-# Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'status', 'ticket_detail_link','created_at', 'updated_at']
+    list_filter = ['status', 'created_at', 'updated_at', 'author']
+    search_fields = ['title', 'body']
+    ordering = ['created_at', 'status']
+
+    def ticket_detail_link(self, obj):
+        url = reverse('admin-ticket-detail', args=[obj.id])
+        return format_html('<a href="{}">View Ticket</a>', url)
