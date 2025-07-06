707cdffe22395206274278aab21a0ae75e1ba394
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 11:44:27 +0200

    feat: add user serializer with first and last name, in ticket serializer add nested serializer author and make that user sees only minimal needed data, but staff see also authors first and last name

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,14 +1,33 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Ticket
 
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
+        read_only_fields = ['first_name', 'last_name']
+
 class TicketSerializer(serializers.ModelSerializer):
+    # Show author for staff users
+    author = serializers.SerializerMethodField()
+
     class Meta:
         model = Ticket
-        fields = ['id', 'title', 'body', 'created_at', 'updated_at', 'status']
+        fields = ['id', 'author', 'title', 'body', 'status', 'created_at', 'updated_at']
         extra_kwargs = {
             'created_at': {'read_only': True},
             'updated_at': {'read_only': True}
         }
+
+    def get_author(self, obj):
+        """Custom method for author field control."""
+        request = self.context.get('request')
+        if request and request.user.is_staff:
+            return UserSerializer(obj.author).data
+        return None
 
     def validate_status(self, value):
         # Prevent invalid transitions (draft to closed or in progress)
@@ -30,5 +49,25 @@ class TicketSerializer(serializers.ModelSerializer):
             
         return data
     
-    # def create(self, validated_data):
-        # Assign user on creating ticket
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        request = self.context.get('request')
+
+        if not request.user.is_staff:
+            # Regular user sees minimal data
+            return {
+                'title': data['title'],
+                'body': data['body'],
+                'status': data['status'],
+                'created_at': data['created_at'],
+                'updated_at': data['updated_at']
+            }
+        else:
+            # Admins see all needed fields
+            data.pop('id', None)
+            data.pop('email', None)
+            return data
