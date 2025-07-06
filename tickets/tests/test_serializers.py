8ed30449e44dcb3c6942eca3237a4699092d842d
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 15:03:43 +0200

    test: fix test serializers to use api request factory when creating ticket with serializer and update missing author field from tests

--- tickets/tests/test_serializers.py
+++ tickets/tests/test_serializers.py
@@ -1,21 +1,29 @@
 import pytest
+from rest_framework.test import APIRequestFactory
 from ..serializers import TicketSerializer
 from ..models import Ticket
 
 @pytest.mark.django_db
-def test_ticket_serializer_valid_data():
+def test_ticket_serializer_valid_data(test_user):
     """Test normal serializer behaviour with valid data."""
+    factory = APIRequestFactory()
+    request = factory.post('/')
+    request.user = test_user
     data = {
         'title': 'Test ticket',
         'body': 'This is a valid test body of a ticket',
         'status': 'O',
     }
-    serializer = TicketSerializer(data=data)
+    serializer = TicketSerializer(
+        data=data,
+        context={'request': request}
+    )
     assert serializer.is_valid(), serializer.errors
     ticket = serializer.save()
     assert ticket.title == data['title']
     assert ticket.body == data['body']
     assert ticket.status == data['status']
+    assert ticket.author == test_user
     assert Ticket.objects.count() == 1
 
 @pytest.mark.django_db
@@ -44,13 +52,14 @@ def test_ticket_serializer_incorrect_status():
     assert Ticket.objects.count() == 0
 
 @pytest.mark.django_db
-def test_ticket_serializer_updating_status():
+def test_ticket_serializer_updating_status(test_user):
     """Test serializer when changing status to correct and then incorrect one."""
     # First create ticket object
     ticket = Ticket.objects.create(
         title='Test title',
         body='Test body',
-        status='D' # Starting with Draft
+        status='D', # Starting with Draft
+        author=test_user
     )
 
     # Draft to Open - valid
