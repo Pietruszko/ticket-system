cb40693f10812c0c3e99ebd0f1296322fb2fefc0
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 11:17:35 +0200

    feat: create admin ticket detail view for admin panel

--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+from rest_framework import generics
 from .models import Ticket
 from .serializers import TicketSerializer
 
@@ -10,4 +12,11 @@ class TicketViewSet(ModelViewSet):
 
     def get_queryset(self):
         return super().get_queryset()
-    
+
+class AdminTicketDetail(generics.RetrieveAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(status='D')
