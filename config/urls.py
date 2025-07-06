5df78eb6a13389bc2d82f29540950dbe56c26af7
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 15:01:52 +0200

    config: add simple jwt to setting and rest framework settings, add paths to create and refresh tokens

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from tickets.views import TicketViewSet, AdminTicketDetail
 
 router = DefaultRouter()
@@ -26,4 +30,6 @@ urlpatterns = [
     path('admin/tickets/<int:pk>/', AdminTicketDetail.as_view(), name='admin-ticket-detail'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/login/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
