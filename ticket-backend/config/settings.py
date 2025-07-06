0b0ffdad81027b67b9bb221cf7fb3747ccafc78e
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 18:43:52 +0200

    config: add corsheaders to installed apps, middleware and add angular port to cors allowed origins

--- ticket-backend/config/settings.py
+++ ticket-backend/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'tickets',
 ]
 
@@ -57,6 +58,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +66,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200', # Angular port
 ]
 
 ROOT_URLCONF = 'config.urls'
