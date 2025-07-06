acf90bce7fc3776dc82c8c1a80e6e4387a3f8bba
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 18:07:34 +0200

    chore: reorganize backend to its own directory, before starting frontend

---
+++ ticket-backend/tickets/views.py
@@ -0,0 +1,30 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import generics
+from .models import Ticket
+from .serializers import TicketSerializer
+
+class TicketViewSet(ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        qs = super().get_queryset().select_related('author')
+
+        # For staff
+        if self.request.user.is_staff:
+            return qs.exclude(status='D')
+        
+        # For regular users
+        return qs.filter(author=self.request.user)
+
+class AdminTicketDetail(generics.RetrieveAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(status='D')
