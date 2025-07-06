02e7cc9ca7cd9e9f52232b9406cacde3e16b7d9d
Pietruszko <gabriel.karalus98@gmail.com>
2025-07-06 20:49:36 +0200

    config: fix permission classes

--- ticket-backend/config/settings.py
+++ ticket-backend/config/settings.py
@@ -50,7 +50,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
